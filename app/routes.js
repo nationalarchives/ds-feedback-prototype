5ec7fb7d9c3bf97b2ccee789b36f2a5dd82a1ac6
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-05-23 16:07:38 +0100

    Initial idea

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,16 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.post('/588c1222-5bdf-40db-9d84-29ffe6e3bb91/prompts/add-option', function (req, res) {
+    var type = req.session.data['type']
+    console.log(type)
+    if (type == "binary"){
+      res.redirect('/588c1222-5bdf-40db-9d84-29ffe6e3bb91/prompts/add/binary')
+    } else if (type == "range"){
+      res.redirect('/588c1222-5bdf-40db-9d84-29ffe6e3bb91/prompts/add/range')
+    } else if (type == "text"){
+      res.redirect('/588c1222-5bdf-40db-9d84-29ffe6e3bb91/prompts/add/text')
+    } else {
+      res.redirect('/588c1222-5bdf-40db-9d84-29ffe6e3bb91/prompts/add')
+    }
+  })
